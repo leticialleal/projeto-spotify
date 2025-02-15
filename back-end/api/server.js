697518b47fb085ae7e18fc24a4584da9f15da103
Leticia Leal <leticialleal28@gmail.com>
2025-02-15 18:35:48 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -10,26 +10,45 @@ import { db } from './connect.js';
 // npm i cors
 import cors from 'cors'
 
+import path from 'path'
+
+const __dirname = path.resolve();
+// console.log(__dirname);
+
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 // tipo POST: app.use(express.json())
 
-// path => endpoint
-app.get('/', (request, response) => {
+app.get('/api/', (request, response) => {
     response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 })
-
+// Antes do deploy
+// path => endpoint
+// app.get('/', (request, response) => {
+//     response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
+// })
+    
 // JavaScript assíncrono: await (espera até promise estar fullfilled) e async
-
-app.get('/artists', async (request, response) => {
+    
+app.get('/api/artists', async (request, response) => {
     response.send(await db.collection('artists').find({}).toArray());
 })
-
-
-app.get('/songs', async (request, response) => {
+// app.get('/artists', async (request, response) => {
+//     response.send(await db.collection('artists').find({}).toArray());
+// })
+    
+app.get('/api/songs', async (request, response) => {
     response.send(await db.collection('songs').find({}).toArray());
+}) 
+// app.get('/songs', async (request, response) => {
+//     response.send(await db.collection('songs').find({}).toArray());
+// })
+
+app.use(express.static(path.join(__dirname, '../front-end/dist')))
+app.get('*', async (request, response) => {
+    response.sendFile(path.join(__dirname, '../front-end/dist/index.html'));
 })
 
 app.listen(PORT, () => {
